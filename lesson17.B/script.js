a73ac72a07bf55bb994e7c369c0847bcfd497a93
unknown <solodzh@gmail.com>
2021-08-30 08:37:00 +0200

    Выполнил задание 24

--- lesson17.B/script.js
+++ lesson17.B/script.js
@@ -1,16 +1,33 @@
-'use strict';
+const countrySelect = document.getElementById('country');
+const citySelect = document.getElementById('city');
+const result = document.querySelector('.result');
 
-let timer;
-const input = document.querySelector('.input');
-const out = document.querySelector('.out');
-const printResult = (text) => {
-  out.textContent = text;
+const cityArr = {
+  rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+  uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+  bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
+  jap: ['Токио', 'Киото', 'Осака', 'Иокогама']
 };
 
-input.addEventListener('input', (e) => {
+const getCity = e => {
 
-  if (timer) {
-    clearTimeout(timer);
+  while (citySelect.firstChild) {
+    citySelect.removeChild(citySelect.firstChild);
   }
-  timer = setTimeout(() => {out.textContent = e.target.value;}, 300);
-});
+  const city = cityArr[e.target.options[e.target.selectedIndex].value];
+  city.forEach(element => {
+    const citySelectElem = document.createElement('option');
+    citySelectElem.setAttribute('value', element);
+    citySelectElem.textContent = element;
+    citySelect.append(citySelectElem);
+    citySelect.style.display = 'inline-block';
+  });
+};
+
+const showResult = () => {
+  result.textContent = countrySelect.options[countrySelect.selectedIndex].textContent + ', ' +
+  citySelect.options[citySelect.selectedIndex].textContent;
+};
+
+countrySelect.addEventListener('change', getCity);
+citySelect.addEventListener('change', showResult);
